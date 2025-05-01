51e5f066bc8c92d338e32e65e43d96d43a1e0be7
hriidzhridoy <amamunhridoy@gmail.com>
2025-05-01 18:06:25 +0600

    route wise res.send

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Server is running... on port 3000");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/about", (req, res) => {
+  res.send("About Us");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
