200d1e03e7180caddc3e4ed6f00ec6483644f75b
hriidzhridoy <amamunhridoy@gmail.com>
2025-05-01 18:31:28 +0600

    understood the env shit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.get("/about", (req, res) => {
   res.send("About Us");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
